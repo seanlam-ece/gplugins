62e11e716b36bf1bf4d8f6ed0876dcf48af5cf79
seanlam97 <seanlam97@gmail.com>
2024-10-08 12:10:31 -0700

    Fix PN junction DEVICE example

--- gplugins/lumerical/examples/example_device.py
+++ gplugins/lumerical/examples/example_device.py
@@ -3,6 +3,7 @@ from functools import partial
 from gplugins.lumerical.simulation_settings import SimulationSettingsLumericalCharge
 from gplugins.lumerical.convergence_settings import LUMERICAL_CHARGE_CONVERGENCE_SETTINGS
 from gplugins.lumerical.device import LumericalChargeSimulation
+from gdsfactory.generic_tech.layer_stack import get_process
 import gdsfactory as gf
 
 # Create curved PN junction
@@ -17,94 +18,13 @@ cross_section_pn = partial(
     width_metal=0.5,
 )
 
-# doped_path = gf.Path()
-# doped_path.append(gf.path.arc(radius=10.0, angle=361))
 p = gf.path.arc(radius=10, angle=135)
 c = p.extrude(cross_section_pn)
-# c << doped_path.extrude(cross_section=cross_section_pn)
 
-### TODO: Update generic PDK with dopants in layer_stack
-from gdsfactory.generic_tech.layer_map import LAYER
 from gdsfactory.pdk import get_layer_stack
-from gdsfactory.technology.layer_stack import LayerLevel
 
+process = get_process()
 layer_stack = get_layer_stack()
-layer_stack.layers["substrate"].info["layer_type"] = "background"
-layer_stack.layers["substrate"].info["background_doping_ion"] = None
-layer_stack.layers["substrate"].info["background_doping_concentration"] = None
-layer_stack.layers["box"].info["layer_type"] = "background"
-layer_stack.layers["clad"].info["layer_type"] = "background"
-layer_stack.layers["core"].sidewall_angle = 0
-layer_stack.layers["slab90"].sidewall_angle = 0
-layer_stack.layers["via_contact"].sidewall_angle = 0
-layer_stack.layers["N"] = LayerLevel(
-    layer=LAYER.N,
-    thickness=0.22,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=5e17,
-    background_doping_ion="n",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-layer_stack.layers["P"] = LayerLevel(
-    layer=LAYER.P,
-    thickness=0.22,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=7e17,
-    background_doping_ion="p",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-layer_stack.layers["NP"] = LayerLevel(
-    layer=LAYER.NP,
-    thickness=0.09,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=3e18,
-    background_doping_ion="n",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-layer_stack.layers["PP"] = LayerLevel(
-    layer=LAYER.PP,
-    thickness=0.09,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=2e18,
-    background_doping_ion="p",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-layer_stack.layers["NPP"] = LayerLevel(
-    layer=LAYER.NPP,
-    thickness=0.09,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=1e19,
-    background_doping_ion="n",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-layer_stack.layers["PPP"] = LayerLevel(
-    layer=LAYER.PPP,
-    thickness=0.09,
-    zmin=0,
-    material="si",
-    mesh_order=4,
-    background_doping_concentration=1e19,
-    background_doping_ion="p",
-    orientation="100",
-    info={"layer_type": "doping"},
-)
-
-# c.show()
 
 # Set up simulation settings
 charge_settings = SimulationSettingsLumericalCharge(x=10, y=10)
@@ -131,6 +51,7 @@ boundary_settings = {
 sim = LumericalChargeSimulation(
     component=c,
     layerstack=layer_stack,
+    process=process,
     simulation_settings=charge_settings,
     convergence_settings=LUMERICAL_CHARGE_CONVERGENCE_SETTINGS,
     boundary_settings=boundary_settings,
