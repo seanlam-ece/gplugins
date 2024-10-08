6e932605b2d24680f4c38ab9b18a1e5f84be3d40
seanlam97 <seanlam97@gmail.com>
2024-10-08 13:08:15 -0700

    Fix FDTD example

--- gplugins/lumerical/examples/example_fdtd.py
+++ gplugins/lumerical/examples/example_fdtd.py
@@ -2,12 +2,11 @@ from functools import partial
 
 import gdsfactory as gf
 from gdsfactory.components.taper_cross_section import taper_cross_section
-from gdsfactory.technology.layer_stack import LayerLevel, LayerStack
 
 from gplugins.lumerical.convergence_settings import LUMERICAL_FDTD_CONVERGENCE_SETTINGS
 from gplugins.lumerical.fdtd import LumericalFdtdSimulation
 from gplugins.lumerical.simulation_settings import SIMULATION_SETTINGS_LUMERICAL_FDTD
-
+from gdsfactory.generic_tech.layer_stack import get_layer_stack, get_process
 
 xs_wg = partial(
     gf.cross_section.cross_section,
@@ -28,87 +27,24 @@ taper = taper_cross_section(
     width_type="parabolic",
 )
 
-layerstack_lumerical2021 = LayerStack(
-    layers={
-        "clad": LayerLevel(
-            name=None,
-            layer=(99999, 0),
-            thickness=3.0,
-            thickness_tolerance=None,
-            zmin=0.0,
-            zmin_tolerance=None,
-            material="sio2",
-            sidewall_angle=0.0,
-            sidewall_angle_tolerance=None,
-            width_to_z=0.0,
-            z_to_bias=None,
-            mesh_order=9,
-            layer_type="background",
-            mode=None,
-            into=None,
-            resistivity=None,
-            bias=None,
-            derived_layer=None,
-            info={},
-            background_doping_concentration=None,
-            background_doping_ion=None,
-            orientation="100",
-        ),
-        "box": LayerLevel(
-            name=None,
-            layer=(99999, 0),
-            thickness=3.0,
-            thickness_tolerance=None,
-            zmin=-3.0,
-            zmin_tolerance=None,
-            material="sio2",
-            sidewall_angle=0.0,
-            sidewall_angle_tolerance=None,
-            width_to_z=0.0,
-            z_to_bias=None,
-            mesh_order=9,
-            layer_type="background",
-            mode=None,
-            into=None,
-            resistivity=None,
-            bias=None,
-            derived_layer=None,
-            info={},
-            background_doping_concentration=None,
-            background_doping_ion=None,
-            orientation="100",
-        ),
-        "core": LayerLevel(
-            name=None,
-            layer=(1, 0),
-            thickness=0.22,
-            thickness_tolerance=None,
-            zmin=0.0,
-            zmin_tolerance=None,
-            material="si",
-            sidewall_angle=10.0,
-            sidewall_angle_tolerance=None,
-            width_to_z=0.5,
-            z_to_bias=None,
-            mesh_order=2,
-            layer_type="grow",
-            mode=None,
-            into=None,
-            resistivity=None,
-            bias=None,
-            derived_layer=None,
-            info={"active": True},
-            background_doping_concentration=100000000000000.0,
-            background_doping_ion="Boron",
-            orientation="100",
-        ),
-        # KNOWN ISSUE: Lumerical 2021 version of Layer Builder does not support dopants in process file
-    }
-)
+layerstack = get_layer_stack()
+process = get_process()
+
+# Remove metal and nitride layers for visualization in Lumerical
+layerstack.layers.pop("nitride")
+layerstack.layers.pop("via_contact")
+layerstack.layers.pop("metal1")
+layerstack.layers.pop("heater")
+layerstack.layers.pop("via1")
+layerstack.layers.pop("metal2")
+layerstack.layers.pop("via2")
+layerstack.layers.pop("metal3")
+
 SIMULATION_SETTINGS_LUMERICAL_FDTD.port_translation = 1.0
 sim = LumericalFdtdSimulation(
     component=taper,
-    layerstack=layerstack_lumerical2021,
+    layerstack=layerstack,
+    process=process,
     convergence_settings=LUMERICAL_FDTD_CONVERGENCE_SETTINGS,
     simulation_settings=SIMULATION_SETTINGS_LUMERICAL_FDTD,
     hide=False,
